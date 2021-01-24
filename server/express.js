4066130f5ef0d3f20e8c1c79c92baa5ef261d01a
Marc Lasserre <marc.lasserre@mconsultingsports.fr>
2021-01-24 21:36:04 +0100

    Add client folder before 'main.js'

--- server/express.js
+++ server/express.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
@@ -8,7 +9,14 @@ import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 
+// Comment out before building for production
+import devBundle from './devBundle'
+
+const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
+
+// Comment out before building for production
+devBundle.compile(app)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,11 +24,15 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.use('/', userRoutes)
+app.use('/', authRoutes)
+
 app.get('/', (req, res) => {
   res.status(200).send(Template())
 })
-app.use('/', userRoutes)
-app.use('/', authRoutes)
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
     res.status(401).json({"error" : err.name + ": " + err.message })
